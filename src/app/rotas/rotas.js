caa893c32d65348a2bb44720c982844c8e723172
Gustavo Araujo <gustavocaraujo@gmail.com>
2020-08-12 22:59:34 -0300

    put-delete

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -18,17 +18,59 @@ module.exports = function(app) {
     });
     
     app.get('/livros', function(req, resp) {
-
         const livroDao = new LivroDao(db);
-        livroDao.lista()
-                .then(livros => resp.marko(
-                    require('../views/livros/lista/lista.marko'),
-                    {
-                        livros: livros
-                    }
-    
-                ))
-                .catch(erro => console.log(erro));
-    });
+        livroDao
+            .lista()
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: livros
+                }
 
+            ))
+            .catch(erro => console.log(erro)
+        );
+    });
+        
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(
+            require('../views/livros/form/form.marko'),
+            { livro: {} }
+        )
+    });
+    
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        livroDao
+            .adiciona(req.body)
+            .then(resp.redirect('/livros'))  
+            .catch(erro => console.log(erro)
+        );
+    });
+    
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        livroDao
+            .atualiza(req.body)
+            .then(resp.redirect('/livros'))  
+            .catch(erro => console.log(erro)
+        );
+    });
+        
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
+    });
 };
