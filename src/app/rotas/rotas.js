2932c4df946dac09360304a3b99a0237540e56a4
Gustavo Araujo <gustavocaraujo@gmail.com>
2020-08-19 09:00:14 -0300

    mvc

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,76 +1,10 @@
-const LivroDao = require('../infra/livro-dao');
+const baseRotas = require('./base-rotas');
+const livroRotas = require('./livro-rotas');
 
-const db = require('../../config/database');
 
-module.exports = function(app) {
+module.exports = (app) => {
 
-    app.get('/', function(req, resp){
-        resp.send(`
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-        `)
-    });
-    
-    app.get('/livros', function(req, resp) {
-        const livroDao = new LivroDao(db);
-        livroDao
-            .lista()
-            .then(livros => resp.marko(
-                require('../views/livros/lista/lista.marko'),
-                {
-                    livros: livros
-                }
+    baseRotas(app);
+    livroRotas(app);
 
-            ))
-            .catch(erro => console.log(erro)
-        );
-    });
-        
-    app.get('/livros/form', function(req, resp) {
-        resp.marko(
-            require('../views/livros/form/form.marko'),
-            { livro: {} }
-        )
-    });
-    
-    app.post('/livros', function(req, resp) {
-        console.log(req.body);
-        const livroDao = new LivroDao(db);
-        livroDao
-            .adiciona(req.body)
-            .then(resp.redirect('/livros'))  
-            .catch(erro => console.log(erro)
-        );
-    });
-    
-    app.put('/livros', function(req, resp) {
-        console.log(req.body);
-        const livroDao = new LivroDao(db);
-        livroDao
-            .atualiza(req.body)
-            .then(resp.redirect('/livros'))  
-            .catch(erro => console.log(erro)
-        );
-    });
-        
-    app.get('/livros/form/:id', function(req, resp) {
-        const id = req.params.id;
-        const livroDao = new LivroDao(db);
-    
-        livroDao.buscaPorId(id)
-            .then(livro => 
-                resp.marko(
-                    require('../views/livros/form/form.marko'),
-                    { livro: livro }
-                )
-            )
-            .catch(erro => console.log(erro));
-    
-    });
 };
